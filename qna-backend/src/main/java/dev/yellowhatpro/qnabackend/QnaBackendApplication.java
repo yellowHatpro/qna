03dcdd0fa6f69c070976c2a0deb466b4a30bd626
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2023-01-23 23:21:04 +0530

    initial setup

---
+++ qna-backend/src/main/java/dev/yellowhatpro/qnabackend/QnaBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.yellowhatpro.qnabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QnaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QnaBackendApplication.class, args);
+    }
+
+}
