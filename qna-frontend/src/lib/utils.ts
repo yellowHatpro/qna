356e6ad0455f8be2d3a92813bbc5e345d93a60fa
yellowhatpro <yellowhatpro3119@gmail.com>
2024-03-08 17:03:11 +0530

    ports stuff

--- qna-frontend/src/lib/utils.ts
+++ qna-frontend/src/lib/utils.ts
@@ -26,4 +26,4 @@ export function toFullDate(unixTime: string) {
 }
 
 export const env = process.env.NODE_ENV === "development" ? "development" : "production"
-export const url = process.env.NODE_ENV === "development" ? "http://localhost:8080" : "https://wa-qna.azurewebsites.net"
+export const url = process.env.NODE_ENV === "development" ? "http://localhost:80" : "https://wa-qna.azurewebsites.net"
